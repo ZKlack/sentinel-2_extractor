e55bf7e7119b21e1c4e41ac1a35296672a231368
ZKlack <xklack@outlook.sa>
2025-10-04 14:28:25 +0300

    added key loading capabilities

--- dockerfile
+++ dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.11-slim
 
 RUN pip install --no-cache-dir sentinelhub
+RUN pip install --no-cache-dir python-dotenv
 
-COPY main.py /main.py
+COPY main.py /app/main.py
+COPY .env /app/.env
+
+WORKDIR /app
 ENTRYPOINT ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from sentinelhub import SHConfig #set config for sentinelhub
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 config = SHConfig()
+config.instance_id = os.getenv('INSTANCE_ID')
+config.sh_client_id = os.getenv('SH_CLIENT_ID')
+config.sh_client_secret = os.getenv('SH_CLIENT_SECRET')
 
 print(config)
